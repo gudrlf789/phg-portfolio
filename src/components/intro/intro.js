144ab4d4aa4b1c7be2661f72fffec64eed3c5573
park hyeong gil <gudrlf7913@naver.com>
2021-11-01 02:19:29 +0900

    intro 파일 i-left 클래스 부분 css 코드 작성 완료

--- src/components/intro/intro.js
+++ src/components/intro/intro.js
@@ -3,7 +3,24 @@ import "./intro.css"
 const Intro = () => {
   return (
     <div className="i">
-      <div className="i-left">left</div>
+      <div className="i-left">
+        <div className="i-left-wrapper">
+          <h2 className="i-intro">Hello, My name is</h2>
+          <h1 className="i-name">Hyeong Gil Park</h1>
+          <div className="i-title">
+            <div className="i-title-wrapper">
+              <div className="i-title-item">Web Developer</div>
+              <div className="i-title-item">REACT</div>
+              <div className="i-title-item">JAVA</div>
+              <div className="i-title-item">JAVA SCRIPT</div>
+              <div className="i-title-item">NODEJS</div>
+            </div>
+          </div>
+          <div className="i-desc">
+            Welcome to phg-protfolio!!
+          </div>
+        </div>
+      </div>
       <div className="i-right">right</div>
     </div>
   )
