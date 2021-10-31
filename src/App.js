606127a30d935c955ae67dbce12353d7eeda2af7
park hyeong gil <gudrlf7913@naver.com>
2021-11-01 01:56:25 +0900

    1.파비콘 및 이미지 추가 2. components,intro 폴더 생성 3. intro.css intro.js 파일 생성 4.불필요한 파일들 삭제

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Intro from './components/intro/intro';
+
+function App() {
+  return (
+    <div className="App">
+      <Intro />
+    </div>
+  );
+}
+
+export default App;
